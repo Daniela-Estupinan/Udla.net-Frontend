8e84c42623d09d572b984d3dec93eb644e92f058
Martin Galvez <martin.galvez@kinanalytics.com>
2023-04-14 19:02:39 -0500

    First commit

---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,50 @@
+import {camera, trash, close} from 'ionicons/icons';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+    IonActionSheet,
+} from '@ionic/react';
+import {usePhotoGallery} from '../hooks/usePhotoGallery';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+
+const Tab2: React.FC = () => {
+    const {photos, takePhoto} = usePhotoGallery();
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Photo Gallery</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonGrid>
+                    <IonRow>
+                        {photos.map((photo, index) => (
+                            <IonCol size="6" key={index}>
+                                <IonImg src={photo.webviewPath} />
+                            </IonCol>
+                        ))}
+                    </IonRow>
+                </IonGrid>
+                <IonFab vertical="bottom" horizontal="center" slot="fixed">
+                    <IonFabButton onClick={() => takePhoto()}>
+                        <IonIcon icon={camera}></IonIcon>
+                    </IonFabButton>
+                </IonFab>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Tab2;
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    exclude: [`@ionic/pwa-elements/loader`],
+  },
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ]
+})
